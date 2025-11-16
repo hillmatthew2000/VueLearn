d3b15ead9bab769677de7cb6c7e2fe06d2e53cde
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-11-16 16:59:40 -0600

    fix: correct import statements and initialize reactive variable in App.vue

--- VueMasterclass2024/src/App.vue
+++ VueMasterclass2024/src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+
+const test = ref()
+
+
 </script>
 
 <template>
