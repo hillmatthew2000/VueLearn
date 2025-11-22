8174a605a194dc0b9054f7a2a0d5773948af62e4
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-11-22 09:16:30 -0600

    style: remove unnecessary semicolons in App.vue and main.ts

--- VueMasterclass2024/src/App.vue
+++ VueMasterclass2024/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue';
-import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
 const test = ref()
 </script>
